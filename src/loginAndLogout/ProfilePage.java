f15aa55c19d54085d9d72621b6d04f467170eab7
Prashant Patil <prashant.patil@swqa-solutions.com>
2024-03-14 15:53:54 +0530

    This is my first commit

---
+++ src/loginAndLogout/ProfilePage.java
@@ -0,0 +1,8 @@
+package loginAndLogout;
+
+public class ProfilePage {
+public void ProfileInfo() {
+	System.out.println("Feating profile INFO ");
+}
+
+}
---
+++ src/loginAndLogout/loginLogoutGitProject.java
@@ -0,0 +1,15 @@
+package loginAndLogout;
+
+public class loginLogoutGitProject {
+
+	
+	public void login() {
+		System.out.println("Login Project");
+	}
+	public void TitelVerification() {
+		System.out.println("Titel Verification  ");
+	}
+	public void Logout() {
+		System.out.println("Logout");
+	}
+}
